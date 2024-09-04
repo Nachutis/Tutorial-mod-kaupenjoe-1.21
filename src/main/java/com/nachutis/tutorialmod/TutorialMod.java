0db3b57765083568edf633aaa991f2ed32a14b5c
Nachutis <nachito3110@outlook.com>
2024-09-03 21:13:01 -0700

    followed video, did something wrong with the texture though

--- src/main/java/com/nachutis/tutorialmod/TutorialMod.java
+++ src/main/java/com/nachutis/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.nachutis.tutorialmod;
 
+import com.nachutis.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize() {
 
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
